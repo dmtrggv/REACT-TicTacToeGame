3bd877118e3bdba878a1f6a4b7ebbec918c9da0f
Dimitar Gogov <dmtrkggv@gmail.com>
2024-09-22 17:51:29 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import TicTacToe from "./components/TicTacToe/TicTacToe.tsx";
+import "./components/TicTacToe/TicTacToe.css";
+
+function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
